b7a1e44d8e9238fc7417245f9c6f0617e78fc02e
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-04-24 14:01:47 -0300

    wip: adjust status

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,8 +6,8 @@ export function Header({ status }: HeaderProps) {
   return (
     <header className="w-screen bg-fuchsia-800 p-10">
       <h1 className="m-auto text-white text-3xl">Lista de Tarefas</h1>
-      <h3 className="m-auto text-white text-xl">Organize sua vida de forma simples e prática</h3>
-      <span className="text-white mr-3">Quantidade de tasks:</span>
+      {/* <h3 className="m-auto text-white text-xl">Organize sua vida de forma simples e prática</h3> */}
+      <span className="text-white mr-3">Quantidade de tarefas:</span>
       <span className="text-white">{status}</span>
     </header>
   )
